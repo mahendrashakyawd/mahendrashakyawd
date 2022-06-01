481b6546e5a7a491cd8a0943f6339fff6455ba0d
mahendra shakya <mahendrashakyawd@gmail.com>
2022-06-01 15:59:41 +0530

    added shopsa

---
+++ shopsa/js/main.js
@@ -0,0 +1,93 @@
+
+
+$('.regular').slick({
+  dots: false,
+  infinite: true,
+   arrows:true,
+  speed: 300,
+  slidesToShow: 6,
+  slidesToScroll: 6,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        infinite: true,
+        dots:false,
+		arrows:true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+$('.regular1').slick({
+  dots: false,
+  infinite: true,
+   arrows:true,
+	 prevArrow: $(".right-slide"),
+    nextArrow: $(".left-slide"),
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        dots:false,
+		arrows:true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+$('.blog').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+	infinite: true,
+    mobileFirst: true,
+    responsive: [
+          {
+                  breakpoint: 767,
+                  settings: 'unslick'
+          }
+    ]
+  });
+
